09ad3d6c8949f8f81e5ddf1129ef14d9ad936d14
TechGlue <garcialuis24@hotmail.com>
2023-10-08 15:41:05 -0700

    found our target html element

---
+++ go.mod
@@ -0,0 +1,5 @@
+module GoSupCommunity
+
+go 1.21.1
+
+require golang.org/x/net v0.16.0
--- main.go
+++ main.go
@@ -3,15 +3,45 @@ import(
   "fmt"
   "net/http"
   "io"
+  "strings"
+  "golang.org/x/net/html"
 )
 
 func main() {
   url := "https://www.supremecommunity.com/season/fall-winter2023/droplist/2023-10-05/"
-  fetchHtml(url)
+  body := fetchHtml(url)
+  parseHTML(body)
 
 }
 
-func fetchHtml(url string) {
+//div catalog-inner is what has the 
+func parseHTML(rawHTML string){
+  tokenizer := html.NewTokenizer(strings.NewReader(rawHTML))
+  for {
+	tokenType := tokenizer.Next()
+  	token := tokenizer.Token()
+
+	//Error handling
+  	if tokenType == html.ErrorToken {
+  	 if tokenizer.Err() == io.EOF {
+  	  return
+  	 }
+  	 fmt.Printf("Error: %v", tokenizer.Err())
+  	 return
+  	}
+
+	switch token.Data{
+	  case "div":
+		for _, a := range token.Attr {
+		  if a.Val == "catalog-inner"{
+			fmt.Printf("Token: %v\n", html.UnescapeString(token.String()))
+		  }
+		}
+	}
+ }
+}
+
+func fetchHtml(url string) string {
   fmt.Println("Fetching HTML from", url, "\n")
   resp, err := http.Get(url)
   if err != nil || resp.StatusCode != 200{
@@ -23,6 +53,7 @@ func fetchHtml(url string) {
 	  fmt.Printf("%s", error)
 	}
 	resp.Body.Close()
-	fmt.Printf("%s\n", body)
+	return string(body)
   }
+  return "ERROR"
 }
