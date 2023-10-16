c1d55ff776971e8bedbfe91acc1a3b9c23b38c70
TechGlue <garcialuis24@hotmail.com>
2023-10-15 22:52:24 -0700

    sql queries

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module GoSupCommunity
 go 1.21.1
 
 require golang.org/x/net v0.16.0
+
+require github.com/mattn/go-sqlite3 v1.14.17 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"	
 	"io"
 	"net/http"
 	"strings"
@@ -17,10 +19,10 @@ type CatalogItem struct {
 }
 
 func main() {
-  url := "https://www.supremecommunity.com/season/fall-winter2023/droplist/2023-10-05/"
-  body := fetchHtml(url)
-  parseHTML(body)
-
+  PrepareSupItemsTable()
+  //url := "https://www.supremecommunity.com/season/fall-winter2023/droplist/2023-10-05/"
+  //body := fetchHtml(url)
+  //parseHTML(body)
 }
 
 //div catalog-inner is what has the 
@@ -49,6 +51,7 @@ func parseHTML(rawHTML string){
 				fmt.Println("Found div")
 			  }
 			  if token.Data == "a"{
+				fmt.Println("Extracted string", token.String())
 				extracHref(html.UnescapeString(token.String()))
 				break
 			  }
@@ -76,6 +79,10 @@ func extracHref(input string) string {
   return "ERROR" 
 } 
 
+func craftURL(suffix string) string{
+  return "https://www.supremecommunity.com" + suffix
+}
+
 func fetchHtml(url string) string {
   fmt.Println("Fetching HTML from", url, "\n")
   resp, err := http.Get(url)
@@ -91,4 +98,59 @@ func fetchHtml(url string) string {
 	return string(body)
   }
   return "ERROR"
+}
+
+func PrepareSupItemsTable(){
+  db, _ := sql.Open("sqlite3", "./supItems.db")
+  createTable, _ := db.Prepare(`
+  CREATE TABLE IF NOT EXISTS items(
+	id INTEGER NOT NULL PRIMARY KEY,
+	ItemName TEXT NOT NULL,
+	Price FLOAT NOT NULL,
+	ItemCategory TEXT NOT NULL,
+	ItemURL TEXT NOT NULL
+  );
+  `)
+  _, err := createTable.Exec()
+  if err != nil{
+	fmt.Println("Error creating items table:", err)
+  }
+  db.Close()
+}
+
+func InsertIntoItemsTable(item CatalogItem){
+  fmt.Println("Inserting into items table")
+  db, _ := sql.Open("sqlite3", "./items.db")
+  createItem, _ := db.Prepare("INSERT INTO items(itemName, price, itemCategory, itemURL) VALUES(?, ?, ?, ?)")
+  _ ,err := createItem.Exec(item.ItemName, item.Price, item.ItemCategory, item.ItemUrl)
+  if err != nil{
+	fmt.Println("Error inserting into items table:", err)
+  }
+  db.Close()
+}
+
+func FetchItemsFromTable(){
+  fmt.Println("Fetching items from table")
+	db, _ := sql.Open("sqlite3", "./supItems.db")
+	rows, _ := db.Query("SELECT * FROM items")
+	var id int 
+	var itemName string
+	var price float64
+	var itemCategory string
+	var itemURL string
+	for rows.Next(){
+	  rows.Scan(&id, &itemName, &price, &itemCategory, &itemURL)
+	  fmt.Println(&id, &itemName, &price, &itemCategory, &itemURL)
+	}
+	db.Close()
+}
+
+func CleanUpTable(){
+  db, _ := sql.Open("sqlite3", "./supItems.db")
+	cleanTable, _ := db.Prepare("DELETE FROM items")
+	_, err := cleanTable.Exec()
+	if err != nil{
+	fmt.Println("Error cleaning items table:", err)
+	}
+	db.Close()
 }
