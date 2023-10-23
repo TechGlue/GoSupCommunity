214218f9b70cd3d5d79f2e29961bc1031efe0199
TechGlue <garcialuis24@hotmail.com>
2023-10-22 19:31:26 -0700

    logic for capturing all catalog items names and links done

--- main.go
+++ main.go
@@ -5,52 +5,72 @@ import (
 	"io"
 	"net/http"
 	"strings"
-	"regexp"
 	"golang.org/x/net/html"
 )
 
 type CatalogItem struct {
+  ItemId string
   ItemName string
-  Price int64 
   ItemCategory string
   ItemUrl string
 }
 
 func main() {
-  url := "https://www.supremecommunity.com/season/fall-winter2023/droplist/2023-10-05/"
+  fetchCatalogItems("https://www.supremecommunity.com/season/fall-winter2023/droplist/2023-10-05/")
+}
+
+func convertCatalogItemsToJSON(items []CatalogItem) string{
+  return "JSON"
+}
+
+//want this to return a list of catalog items, and be kind of like the main driver
+func fetchCatalogItems(url string) []CatalogItem{
   body := fetchHtml(url)
-  parseHTML(body)
+  return parseHTML(body)
 }
 
 //div catalog-inner is what has the 
-func parseHTML(rawHTML string){
+func parseHTML(rawHTML string) []CatalogItem {
+  var items []CatalogItem
   tokenizer := html.NewTokenizer(strings.NewReader(rawHTML))
+
   for {
 	tokenType := tokenizer.Next()
   	token := tokenizer.Token()
 
-	//Error handling
   	if tokenType == html.ErrorToken {
   	 if tokenizer.Err() == io.EOF {
-  	  return
+	   fmt.Println("EOF")
+	   return items
   	 }
   	 fmt.Printf("Error: %v", tokenizer.Err())
-  	 return
+	 return items
   	}
 
 	switch token.Data{
 	  case "div":
 		for _, a := range token.Attr {
 		  if a.Val == "catalog-inner"{
-			//loop through child tokens until we find div with class of catalog-list
 			for {
-			  if token.Data == "div"{
-				fmt.Println("Found div")
-			  }
 			  if token.Data == "a"{
-				fmt.Println("Extracted string", token.String())
-				extracHref(html.UnescapeString(token.String()))
-				break
+				var item CatalogItem
+				for _, a := range token.Attr {
+				  fmt.Println(a.Key, a.Val)
+				  if a.Val == "Go to home" {
+				    fmt.Println("Found the home button, indicating the end of the catalog")
+				    return items
+				  }
+				  switch a.Key {
+				    case "href":
+					 item.ItemUrl = craftURL(a.Val)
+					case "data-itemid":
+					  item.ItemId = a.Val 
+					case "data-itemname":
+					  item.ItemName = a.Val
+				  }
+				  items = append(items, item)
+				  break
+				}
 			  }
 			  tokenType = tokenizer.Next()
 			  token = tokenizer.Token()
@@ -60,21 +80,6 @@ func parseHTML(rawHTML string){
 	}
  }
 }
-
-func extracHref(input string) string {
-  hrefRegex := regexp.MustCompile(`<a\s+[^>]*href="([^"]+)"[^>]*>`)
-  matches := hrefRegex.FindAllStringSubmatch(input, -1)
-
-  // Extract and print href values
-  for _, match := range matches {
-  	if len(match) >= 2 {
-  		href := match[1]
-  		fmt.Println("Href:", href)
-		return href
-  	}
-  }
-  return "ERROR" 
-} 
 
 func craftURL(suffix string) string{
   return "https://www.supremecommunity.com" + suffix
